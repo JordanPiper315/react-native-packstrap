37297f13e1d9bdd135b0ac231004c09eac0bccc4
Jordan Piper <jordanannpiper@gmail.com>
2023-01-28 09:07:59 -0800

    working on making button

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,11 +1,32 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { ImageBackground, StyleSheet, View } from "react-native";
-import { Button, Image, Text } from "react-native-elements";
+import {
+  ImageBackground,
+  StyleSheet,
+  View,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { Button, Text } from "react-native-elements";
+import { useState } from "react";
 
 const backgroundImage = require("../assets/images/welcomeImage.jpg");
 
 const WelcomeScreen = () => {
+  const [showButton, setShowButton] = useState(true);
+  const [backgroundImageChange, setBackgroundImageChange] = useState(false);
+
+  const handleWelcome = () => {
+    showButton(!setShowButton);
+    {
+      /* when clicked steps:
+      animate backgrond image to bounce up or fade
+      reveals 2 new buttons - login sign up
+      with 2 fields username and password
+  */
+    }
+  };
+
   return (
     <View style={styles.container}>
       <ImageBackground
@@ -13,15 +34,17 @@ const WelcomeScreen = () => {
         style={styles.backgroundImageStyle}
       >
         <StatusBar style="auto" />
-        <Button
-          title="Welcome"
-          style={styles.button}
-          buttonStyle={{
-            backgroundColor: "rgba(111, 202, 186, 1)",
-            borderRadius: 5,
-            width: 200,
-          }}
-        />
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            title="Welcome"
+            style={styles.button}
+            onPress={() => {
+              console.log("Button Pressed");
+            }}
+          >
+            <Text style={{ alignSelf: "center" }}>Welcome</Text>
+          </TouchableOpacity>
+        </View>
       </ImageBackground>
     </View>
   );
@@ -35,9 +58,18 @@ const styles = StyleSheet.create({
     width: "100%",
     alignItems: "center",
     height: "100%",
+    zIndex: 2,
   },
   button: {
-    paddingBottom: 10,
+    backgroundColor: "rgba(111, 202, 186, 1)",
+    borderRadius: 5,
+    width: 200,
+    height: 50,
+  },
+  buttonContainer: {
+    position: "absolute",
+    bottom: 100,
+    zIndex: 3,
   },
   container: {
     flex: 1,
