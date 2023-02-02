992217bf6cd670da40d875f42f64532d5bb2c481
Jordan Piper <jordanannpiper@gmail.com>
2023-02-01 18:42:40 -0800

    updated homescreen to display icons in cards

--- features/trips/RenderTrip.js
+++ features/trips/RenderTrip.js
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import { Card } from "react-native-elements";
 
 const RenderTrip = ({ trip }) => {
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,42 +1,73 @@
-import { Text, View, ScrollView } from "react-native";
+import {
+  Text,
+  View,
+  ScrollView,
+  Image,
+  FlatList,
+  StyleSheet,
+} from "react-native";
 import { useState } from "react";
-import { Card } from "react-native-elements";
+import { Card, ListItem } from "react-native-elements";
 import { HOMEPAGECARDS } from "../shared/homepagecards";
+import { baseUrl } from "../shared/baseUrl";
 
-const RenderCards = ({ item }) => {
-  if (item) {
-    return (
-      <Card containerStyle={{ padding: 0 }}>
-        <Card.Image source={item.image}>
-          <View style={{ justifyContent: "center", flex: 1 }}>
-            <Text
-              style={{
-                color: "white",
-                textAlign: "center",
-                fontSize: 20,
-              }}
-            >
-              {item.name}
-            </Text>
-          </View>
-        </Card.Image>
-        <Text style={{ margin: 20 }}>{item.description}</Text>
-      </Card>
-    );
-  }
-  return <View />;
-};
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
 
 const HomeScreen = () => {
-  const [cards, setCards] = useState(HOMEPAGECARDS);
-  console.log(cards);
-  const featCards = cards.find((item) => item.name);
+  const cards = HOMEPAGECARDS;
   return (
-    <ScrollView>
-      <Text>Welcome</Text>
-      <RenderCards item={featCards} />
-    </ScrollView>
+    <View style={styles.container}>
+      <Text style={styles.header}>Welcome</Text>
+      <FlatList
+        style={styles.cards}
+        data={cards}
+        renderItem={({ item }) => (
+          <View>
+            <Image
+              source={item.image}
+              style={{
+                width: 100,
+                height: 100,
+                borderWidth: 2,
+                borderColor: "#d34567",
+                margin: 8,
+              }}
+            />
+            <Item title={item.name} />
+          </View>
+        )}
+        keyExtractor={(item) => item.id}
+        numColumns={2}
+        horizontal={false}
+      />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+  },
+  header: {
+    fontSize: 40,
+    justifyAlign: "center",
+    alignItems: "center",
+  },
+  item: {
+    backgroundColor: "#008080",
+    padding: 2,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
 
 export default HomeScreen;
