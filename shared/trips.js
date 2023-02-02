033c44477fd151867e8b4f438d86097d6e5d9b73
Jordan Piper <jordanannpiper@gmail.com>
2023-02-02 07:05:01 -0800

    updated gear and trips data list

--- shared/trips.js
+++ shared/trips.js
@@ -7,6 +7,7 @@ export const TRIPS = [
     featured: false,
     description:
       "Try this 18.6-mile out-and-back trail near Weaverville, California. Generally considered a challenging route, it takes an average of 8 h 31 min to complete. This is a very popular area for backpacking, camping, and fishing, so you'll likely encounter other people while exploring. The best times to visit this trail are May through October. Dogs are welcome, but must be on a leash.",
+    date: "2023-02-14",
   },
   {
     id: 1,
@@ -16,5 +17,16 @@ export const TRIPS = [
     featured: false,
     description:
       "Discover this 27.0-mile out-and-back trail near Weaverville, California. Generally considered a moderately challenging route, it takes an average of 12 h 31 min to complete. This is a popular trail for backpacking, camping, and fishing, but you can still enjoy some solitude during quieter times of day. The best times to visit this trail are June through October. Dogs are welcome and may be off-leash in some areas.",
+    date: "2023-02-01",
+  },
+  {
+    id: 2,
+    name: "Lost Coast Trail: Mattole to Black Sands Beach",
+    image: require("../assets/images/lostCoastTrail.jpeg"),
+    elevation: 1578,
+    featured: false,
+    description:
+      "Get to know this 25.3-mile point-to-point trail near Ferndale, California. Generally considered a moderately challenging route, it takes an average of 8 h 48 min to complete. This is a very popular area for backpacking, birding, and camping, so you'll likely encounter other people while exploring. The trail is open year-round and is beautiful to visit anytime. Dogs are welcome and may be off-leash in some areas.",
+    date: "2023-07-04",
   },
 ];
