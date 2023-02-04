e4385922b4ea27da514001656f8c7d1554215dbe
Jordan Piper <jordanannpiper@gmail.com>
2023-02-03 21:04:04 -0800

    added calendar and moment library

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,8 +1,24 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useState } from "react";
+import { Text, StyleSheet, View } from "react-native";
+import CalendarPicker from "react-native-calendar-picker";
 
 const CalendarScreen = () => {
-  return <Text>Blue</Text>;
+  const [date, setDate] = useState(null);
+  return (
+    <View style={styles.container}>
+      <CalendarPicker onDateChange={setDate} />
+      <View>
+        <Text>SELECTED DATE:{date}</Text>
+      </View>
+    </View>
+  );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    marginTop: 100,
+  },
+});
 export default CalendarScreen;
