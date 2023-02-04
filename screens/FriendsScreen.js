3e7389e0d717595d32e5214087492272588bc0db
Jordan Piper <jordanannpiper@gmail.com>
2023-02-04 09:08:19 -0800

    added friends screen - working on getting avatar image to render

--- screens/FriendsScreen.js
+++ screens/FriendsScreen.js
@@ -1,8 +1,42 @@
 import React from "react";
-import { Text } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { ListItem } from "react-native-elements";
+import { FRIENDS } from "../shared/friendsList";
+
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
 
 const FriendsScreen = () => {
-  return <Text>Friends</Text>;
+  return (
+    <View>
+      <Text style={{ fontSize: 30 }}>Friends</Text>
+      <FlatList
+        data={FRIENDS}
+        renderItem={({ item }) => (
+          <Item title={`${item.name}`} avatar={item.image} />
+        )}
+        keyExtractor={(item) => item.id}
+      />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  item: {
+    borderWidth: 3,
+    backgroundColor: "#008080",
+    padding: 5,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 16,
+  },
+});
 
 export default FriendsScreen;
---
+++ shared/friendsList.js
@@ -0,0 +1,37 @@
+export const FRIENDS = [
+  {
+    id: 0,
+    name: "Josh Sterns",
+    image: require("../assets/images/avatar1.jpg"),
+  },
+  {
+    id: 1,
+    name: "Purple Sterns",
+    image: require("../assets/images/avatar1.jpg"),
+  },
+  {
+    id: 2,
+    name: "Blue People",
+    image: require("../assets/images/avatar2.jpg"),
+  },
+  {
+    id: 3,
+    name: "Rush Now",
+    image: require("../assets/images/avatar2.jpg"),
+  },
+  {
+    id: 4,
+    name: "Green Bex",
+    image: require("../assets/images/avatar1.jpg"),
+  },
+  {
+    id: 5,
+    name: "Becca Lynn",
+    image: require("../assets/images/avatar1.jpg"),
+  },
+  {
+    id: 6,
+    name: "T-rex",
+    image: require("../assets/images/avatar2.jpg"),
+  },
+];
